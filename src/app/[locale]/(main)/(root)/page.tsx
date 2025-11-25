27f710223375c3ad48ef6773ae186aaa1c62beb1
Abdel-Rahman112000 <abdelrahman30688@gmail.com>
2025-11-26 00:27:06 +0200

    add partners slider

--- src/app/[locale]/(main)/(root)/page.tsx
+++ src/app/[locale]/(main)/(root)/page.tsx
@@ -2,9 +2,15 @@ import LayoutStack from "@/layouts/main/layout-stack";
 import MainPageContent from "@/layouts/main/page-content";
 import PageSection from "@/layouts/main/page-section";
 import HeroView from "@/sections/home/hero/hero-view";
+import PartnersView from "@/sections/home/partners/partners-view";
 
 function HomePage() {
-  return <HeroView />;
+  return (
+    <>
+      <HeroView />
+      <PartnersView />
+    </>
+  );
 }
 
 export default HomePage;

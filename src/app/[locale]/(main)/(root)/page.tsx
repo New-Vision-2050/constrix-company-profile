c96e61cedf711b5257901665cab39fc87094abaf
Abdel-Rahman112000 <abdelrahman30688@gmail.com>
2025-11-26 17:36:43 +0200

    add services

--- src/app/[locale]/(main)/(root)/page.tsx
+++ src/app/[locale]/(main)/(root)/page.tsx
@@ -3,12 +3,14 @@ import MainPageContent from "@/layouts/main/page-content";
 import PageSection from "@/layouts/main/page-section";
 import HeroView from "@/sections/home/hero/hero-view";
 import PartnersView from "@/sections/home/partners/partners-view";
+import ServicesView from "@/sections/home/services/services-view";
 
 function HomePage() {
   return (
     <>
       <HeroView />
       <PartnersView />
+      <ServicesView />
     </>
   );
 }
--- src/messages/groups/home/index.ts
+++ src/messages/groups/home/index.ts
@@ -2,9 +2,30 @@ import { _m, MessagesGroup } from "../../types";
 
 export const homeMessages = new MessagesGroup({
   more: _m("More", "المزيد"),
-  tagline: _m(
-    "ENGINEERING & PROJECT MANAGEMENT",
-    "الهندسة وإدارة المشاريع"
-  ),
+  tagline: _m("ENGINEERING & PROJECT MANAGEMENT", "الهندسة وإدارة المشاريع"),
+  partners: _m("Partners", "الشركاء"),
+  servicesTitle: _m("Our Services", "ابرز خدماتنا"),
+  services: new MessagesGroup({
+    service1: new MessagesGroup({
+      title: _m("Supervision of Execution", "الاشراف على التنفيذ"),
+      description: _m(
+        "The official resorts to access all settings and configuration operations and customization. Proceed to add users and manage aliases for group accounts and set policies for supervising email content for business and much more.",
+        "الذي يلجأ إليه المسؤول للوصول إلى كل الإعدادات وعمليات التكوين والتخصيص. اعمد إلى إضافة المستخدمين وإدارة الأسماء المستعارة لحسابات المجموعات وإعداد السياسات للإشراف على محتوى البريد الإلكتروني الخاص بالأعمال والكثير غير ذلك."
+      ),
+    }),
+    service2: new MessagesGroup({
+      title: _m("Supervision of Execution", "الاشراف على التنفيذ"),
+      description: _m(
+        "The official resorts to access all settings and configuration operations and customization. Proceed to add users and manage aliases for group accounts and set policies for supervising email content for business and much more.",
+        "الذي يلجأ إليه المسؤول للوصول إلى كل الإعدادات وعمليات التكوين والتخصيص. اعمد إلى إضافة المستخدمين وإدارة الأسماء المستعارة لحسابات المجموعات وإعداد السياسات للإشراف على محتوى البريد الإلكتروني الخاص بالأعمال والكثير غير ذلك."
+      ),
+    }),
+    service3: new MessagesGroup({
+      title: _m("Supervision of Execution", "الاشراف على التنفيذ"),
+      description: _m(
+        "The official resorts to access all settings and configuration and customization. Proceed to add users and manage aliases for group accounts and set policies for email content for business and much more.",
+        "الذي يلجأ إليه المسؤول للوصول إلى كل الإعدادات التكوين والتخصيص. اعمد إلى إضافة المستخدمين وإدارة الأسماء المستعارة لحسابات المجموعات وإعداد السياسات محتوى البريد الإلكتروني الخاص بالأعمال والكثير"
+      ),
+    }),
+  }),
 });
-
